edec0a672161bccb740a3c4667382db808239a50
jkrayg <jk1019602@gmail.com>
2025-02-28 23:22:18 -0500

    wdbc data test

--- src/com/JakeKrayger/nn/Main.java
+++ src/com/JakeKrayger/nn/Main.java
@@ -1,6 +1,5 @@
 package src.com.JakeKrayger.nn;
 
-
 // Jake Krayger
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -25,82 +24,81 @@ import src.com.JakeKrayger.nn.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
-        // String filePath = "src\\resources\\datasets\\wdbc.data";
-        // ArrayList<double[]> data = new ArrayList<>();
-        // ArrayList<String> l = new ArrayList<>();
-        // SimpleMatrix d;
-        // int rows = 0;
-        // int cols = 0;
+        String filePath = "src\\resources\\datasets\\wdbc.data";
+        ArrayList<double[]> dta = new ArrayList<>();
+        ArrayList<String> lbls = new ArrayList<>();
+        int rows = 0;
+        int cols = 0;
 
-        // try {
-        //     File f = new File(filePath);
-        //     Scanner scan = new Scanner(f);
-        //     while (scan.hasNextLine()) {
-        //         rows += 1;
-        //         String line = scan.nextLine();
-        //         String[] splitLine = line.split(",", 3);
-        //         String label = splitLine[1];
-        //         l.add(label);
-        //         double[] toDub;
-        //         String values = splitLine[2];
-        //         String[] splitValues = values.split(",");
-        //         ArrayList<String> strings = new ArrayList<>(Arrays.asList(splitValues));
-        //         toDub = new double[splitValues.length];
-        //         for (int i = 0; i < splitValues.length; i++) {
-        //             toDub[i] = Double.parseDouble(splitValues[i]);
-        //         }
+        try {
+            File f = new File(filePath);
+            Scanner scan = new Scanner(f);
+            while (scan.hasNextLine()) {
+                rows += 1;
+                String line = scan.nextLine();
+                String[] splitLine = line.split(",", 3);
+                String label = splitLine[1];
+                lbls.add(label);
+                double[] toDub;
+                String values = splitLine[2];
+                String[] splitValues = values.split(",");
+                toDub = new double[splitValues.length];
 
-        //         if (cols == 0) {
-        //             cols = splitValues.length;
-        //         }
-        //         data.add(toDub);
+                for (int i = 0; i < splitValues.length; i++) {
+                    toDub[i] = Double.parseDouble(splitValues[i]);
+                }
 
+                dta.add(toDub);
+            }
 
-        //     }
+            scan.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
-        //     d = new SimpleMatrix(rows, cols);
+        cols = dta.get(0).length;
 
-        //     for (int i = 0; i < data.size(); i++) {
-        //         d.setRow(i, (new SimpleMatrix(data.get(i)).transpose()));
-        //     }
-        //     System.out.println(d.getNumCols());
+        String[] labs = new String[8];
+        double[][] dats = new double[8][cols];
 
-        //     // for (int j = d.getNumRows(); j >= d.getNumRows() - 6; j--) {
-        //     //     System.out.println(d.getRow(j));
-        //     // }
+        for (int i = 0; i < 8; i++) {
+            Random rand = new Random();
+            int r = rand.nextInt(rows);
+            dats[i] = dta.get(r);
+            labs[i] = lbls.get(r);
+        }
 
-        //     // System.out.println();
-        //     // System.out.println(Arrays.asList(labels));
-        //     scan.close();
-        // } catch (FileNotFoundException e) {
-        //     e.printStackTrace();
-        // }
-
-
-        Random random = new Random();
-        double[] sample1 = new double[]{random.nextDouble() * 50, random.nextDouble() * 50, random.nextDouble() * 50};
-        double[] sample2 = new double[]{random.nextDouble() * 50, random.nextDouble() * 50, random.nextDouble() * 50};
-        double[] sample3 = new double[]{random.nextDouble() * 50, random.nextDouble() * 50, random.nextDouble() * 50};
-        double[] sample4 = new double[]{random.nextDouble() * 50, random.nextDouble() * 50, random.nextDouble() * 50};
-        double[] sample5 = new double[]{random.nextDouble() * 50, random.nextDouble() * 50, random.nextDouble() * 50};
-        double[][] sampleData = new double[][]{sample1, sample2, sample3,sample4, sample5};
-        String[] labels = {"T", "F", "T", "T", "F"};
+        // Random random = new Random();
+        // double[] sample1 = new double[]{random.nextDouble() * 50, random.nextDouble()
+        // * 50, random.nextDouble() * 50};
+        // double[] sample2 = new double[]{random.nextDouble() * 50, random.nextDouble()
+        // * 50, random.nextDouble() * 50};
+        // double[] sample3 = new double[]{random.nextDouble() * 50, random.nextDouble()
+        // * 50, random.nextDouble() * 50};
+        // double[] sample4 = new double[]{random.nextDouble() * 50, random.nextDouble()
+        // * 50, random.nextDouble() * 50};
+        // double[] sample5 = new double[]{random.nextDouble() * 50, random.nextDouble()
+        // * 50, random.nextDouble() * 50};
+        // double[][] sampleData = new double[][]{sample1, sample2, sample3,sample4,
+        // sample5};
+        // String[] labels = {"T", "F", "T", "T", "F"};
         // Set<String> classes = new HashSet<>(List.of(labels));
+        // Data data = new Data(sampleData, labels);
+        // data.zScoreNormalization();
 
-        MathUtils maths = new MathUtils();
-        Data data = new Data(sampleData, labels);
+        // System.out.println("Scaled input data: \n" + data.getData());
+        Data data = new Data(dats, labs);
         data.zScoreNormalization();
 
-        System.out.println("Scaled input data: \n" + data.getData());
-
         NeuralNet nn = new NeuralNet();
-        Dense d1 = new Dense(3, new ReLU(), 3);
-        Dense d2 = new Dense(4, new ReLU());
+        Dense d1 = new Dense(6, new ReLU(), 30);
+        Dense d2 = new Dense(7, new ReLU());
         Dense d3 = new Dense(1, new Sigmoid());
         nn.addLayer(d1);
         nn.addLayer(d2);
         nn.addLayer(d3);
 
+        MathUtils maths = new MathUtils();
         ReLU relu = new ReLU();
         Softmax softmax = new Softmax();
         Sigmoid sigmoid = new Sigmoid();
@@ -114,21 +112,22 @@ public class Main {
         d2.setActivations(d2Act);
 
         SimpleMatrix d3Act = sigmoid.execute(maths.weightedSum(d2, d3));
-        System.out.println("d3 activation matrix after Sigmoid function: \n" + d3Act);
+        System.out.println("d3 activation matrix after Sigmoid function: \n" +
+        d3Act);
         d3.setActivations(d3Act);
 
         // nn.compile(new Adam(), new BinCrossEntropy());
 
         BinCrossEntropy bce = new BinCrossEntropy();
+        System.out.println("value after binary cross entropy:");
         System.out.println(bce.execute(d3.getActivations(), data.getLabels()));
 
+        System.out.println("\nclasses:");
         System.out.println(data.getClasses());
+        System.out.println("\nlabels:");
         for (int i = 0; i < data.getLabels().length; i++) {
-            System.out.println(data.getLabels()[i]);
+        System.out.println(data.getLabels()[i]);
         }
-
-
-
 
     }
 }
